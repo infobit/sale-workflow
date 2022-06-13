aa460eaf7138947e71705e8c5dc92dba2d1bf30f
saul <saul@infobit.es>
2022-06-13 16:27:29 +0000

    [FIX]json impuestos en pedido de venta

--- sale_global_discount/models/sale_order.py
+++ sale_global_discount/models/sale_order.py
@@ -1,7 +1,9 @@
 # Copyright 2020 Tecnativa - David Vidal
 # Copyright 2020 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from distutils.log import WARN
 from functools import partial
+import json
 
 from odoo import _, api, exceptions, fields, models
 from odoo.tools.misc import formatLang
@@ -184,3 +186,19 @@ class SaleOrder(models.Model):
                     )
                 )
             order.amount_by_group = res
+
+    @api.depends('order_line.tax_id', 'order_line.price_unit', 'amount_total', 'amount_untaxed')
+    def _compute_tax_totals_json(self):
+        def compute_taxes(order_line):
+            price = order_line.price_unit * (1 - (order_line.discount or 0.0) / 100.0)
+            order = order_line.order_id
+            if order.global_discount_ids:
+                for discount in order.global_discount_ids:
+                    price = price * (1 - (discount.discount or 0.0) / 100.0)
+            return order_line.tax_id._origin.compute_all(price, order.currency_id, order_line.product_uom_qty, product=order_line.product_id, partner=order.partner_shipping_id)
+
+        account_move = self.env['account.move']
+        for order in self:
+            tax_lines_data = account_move._prepare_tax_lines_data_for_totals_from_object(order.order_line, compute_taxes)
+            tax_totals = account_move._get_tax_totals(order.partner_id, tax_lines_data, order.amount_total, order.amount_untaxed, order.currency_id)
+            order.tax_totals_json = json.dumps(tax_totals)
