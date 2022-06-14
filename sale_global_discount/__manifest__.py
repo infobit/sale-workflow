47e957aed274a489b7140734144ff2c245e78ffb
saul <saul@infobit.es>
2022-06-14 08:57:14 +0000

    [NEW]global discount in portal sale order

--- sale_global_discount/__manifest__.py
+++ sale_global_discount/__manifest__.py
@@ -11,7 +11,8 @@
     "license": "AGPL-3",
     "depends": ["account_global_discount", "sale"],
     "data": ["views/sale_order_views.xml", 
-            "views/report_sale_order.xml"
+            "views/report_sale_order.xml",
+            "views/sale_order_portal_content.xml"
         ],
     "application": False,
     "installable": True,
